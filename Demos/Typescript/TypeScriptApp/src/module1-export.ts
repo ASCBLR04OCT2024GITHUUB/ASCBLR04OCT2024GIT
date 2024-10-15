cf16968e3921e898b63524433976a01c572441dc
GenieFamily <IrvynsGenie@gmail.com>
2024-10-15 17:27:41 +0530

    commit 😊

---
+++ Demos/Typescript/TypeScriptApp/src/module1-export.ts
@@ -0,0 +1,9 @@
+export function sum(a: number, b: number): number {
+        return a + b;
+    }
+
+    export function divide(a: number, b: number): number {
+        return a / b;
+    }
+
+    //try multiply function
---
+++ Demos/Typescript/TypeScriptApp/src/module2-import.ts
@@ -0,0 +1,6 @@
+import { sum,divide } from "./module1-export";
+// import { divide } from "./module1-export";
+const result = sum(5, 10);
+console.log(result); // Output: 15
+const result1 = divide(10, 2);
+console.log(result1); // Output: 5
